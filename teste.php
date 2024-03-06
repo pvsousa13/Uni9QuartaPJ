acd9e3d8657264991dd69ea1f35ca59fdf9be240
pvsousa13 <149569320+pvsousa13@users.noreply.github.com>
2024-03-06 19:18:54 -0300

    Update Nomes.php

--- teste.php
+++ teste.php
@@ -1,1 +1,15 @@
+Definir as equipes (Nome e RA)
 
+Leonardo Luna RA: 2223203106
+
+Pedro Vinicius de Sousa RA: 2223203484
+
+Andressa da Silva Souza RA: 2223109601
+
+Danielle Alves maia RA: 2223201324
+
+Claudinei ramos de souza RA: 2223203180
+
+Pedro Sales Pereira de Gois RA: 3023200669
+
+Jefferson Gonçalves De Miranda RA: 2223203778
